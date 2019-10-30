75ce0e4d4431b2bbf9cc863df30505d82f413836
ajdevops1140 <ajdevops1140@gmail.com>
2019-10-30 14:31:59 -0500

    2 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,11 @@ import { HelloComponent } from './hello.component';
 import { FirstLevelComponent } from './first-level/first-level.component';
 import { ChildOfFirstComponent } from './child-of-first/child-of-first.component';
 import { InputTransactionComponent } from './input-transaction/input-transaction.component';
+import { TransactionEntryFormComponent } from './transaction-entry-form/transaction-entry-form.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, FirstLevelComponent, ChildOfFirstComponent, InputTransactionComponent ],
+  declarations: [ AppComponent, HelloComponent, FirstLevelComponent, ChildOfFirstComponent, InputTransactionComponent, TransactionEntryFormComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/transaction-entry-form/transaction-entry-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-transaction-entry-form',
+  templateUrl: './transaction-entry-form.component.html',
+  styleUrls: ['./transaction-entry-form.component.css']
+})
+export class TransactionEntryFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
