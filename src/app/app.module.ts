5cc16dae34f172285a989c43041c1dddf4398dc4
ajdevops1140 <ajdevops1140@gmail.com>
2019-10-31 08:30:41 -0500

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,11 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { FirstLevelComponent } from './first-level/first-level.component';
 import { ChildOfFirstComponent } from './child-of-first/child-of-first.component';
-import { InputTransactionComponent } from './input-transaction/input-transaction.component';
 import { TransactionEntryFormComponent } from './transaction-entry-form/transaction-entry-form.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, FirstLevelComponent, ChildOfFirstComponent, InputTransactionComponent, TransactionEntryFormComponent ],
+  declarations: [ AppComponent, HelloComponent, FirstLevelComponent, ChildOfFirstComponent, TransactionEntryFormComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
