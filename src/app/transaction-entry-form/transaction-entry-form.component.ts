16c51454f3cf0e974fa9cbd0000d69f6a7bbba9b
ajdevops1140 <ajdevops1140@gmail.com>
2019-10-31 15:38:49 -0500

    2 files modified

--- src/app/transaction-entry-form/transaction-entry-form.component.ts
+++ src/app/transaction-entry-form/transaction-entry-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { transactionItem } from '../transactionItem';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-transaction-entry-form',
@@ -9,9 +10,15 @@ import { transactionItem } from '../transactionItem';
 export class TransactionEntryFormComponent implements OnInit {
   transModel:transactionItem = new transactionItem();
  
+  date1:string;
+
   constructor() { }
 
   ngOnInit() {
+    let nD:Date = new Date();    
+    let d = nD.toISOString().substring(0,10);
+
+    this.date1 = d;//.toLocaleString('en-US', { year:'numeric',month:'2-digit', day:'2-digit', });
   }
 
   get diagnostic() { return JSON.stringify(this.transModel); }
