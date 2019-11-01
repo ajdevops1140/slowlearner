9e86e92ef7549a7061043edc66ef1e879161c23e
ajdevops1140 <ajdevops1140@gmail.com>
2019-11-01 08:07:28 -0500

    1 file modified

--- src/app/transactionItem.ts
+++ src/app/transactionItem.ts
@@ -1,10 +1,9 @@
-import { CurrencyPipe } from '@angular/common';
 
 export class transactionItem{
 
   constructor(
     public id?:Number,
-    public date?:Date,
+    public date?:string,
     public desc?:string,
     public amount?:Number
   )
